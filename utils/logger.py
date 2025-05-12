6bc0a8b0d7201741fb0906ce833bfe9d5d59693a
MKR0005 <140781976+MKR0005@users.noreply.github.com>
2025-05-12 13:13:26 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import logging.handlers
 
 class Logger:
     def __init__(self):
@@ -8,17 +9,19 @@ class Logger:
         log_file = os.path.join(log_dir, "app.log")
 
         self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.DEBUG)  # You can change this level dynamically based on environment
         self.logger.propagate = False
 
         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-        file_handler = logging.FileHandler(log_file)
+        # Set up log rotation to avoid large log files
+        file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=10*1024*1024, backupCount=5)
         file_handler.setFormatter(formatter)
 
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
 
+        # Add handlers to logger if not already added
         if not self.logger.handlers:
             self.logger.addHandler(file_handler)
             self.logger.addHandler(stream_handler)
@@ -35,5 +38,6 @@ class Logger:
     def log_error(self, message: str):
         self.logger.error(message)
 
-    def log_critical(self, message: str):
-        self.logger.critical(message)
+    def log_exception(self, ex: Exception):
+        """Log exception with traceback."""
+        self.logger.error("Exception occurred", exc_info=ex)
