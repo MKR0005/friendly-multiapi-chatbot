55e051b90107b4bfb115d924b04075405ec25798
MKR0005 <140781976+MKR0005@users.noreply.github.com>
2025-05-12 11:32:57 +0530

    Update data_manager.py

--- modules/data_manager.py
+++ modules/data_manager.py
@@ -1,19 +1,22 @@
 import os
 import json
+from utils.logger import Logger
 
 class DataManager:
     def __init__(self):
-        self.responses_dir = os.path.join(os.getcwd(), "data", "responses")
+        self.logger = Logger()
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.responses_dir = os.path.join(base_dir, "..", "data", "responses")
         os.makedirs(self.responses_dir, exist_ok=True)
 
     def save_response(self, api_name: str, response_data: dict):
         try:
             file_path = os.path.join(self.responses_dir, f"{api_name}.json")
             with open(file_path, "w") as file:
-                json.dump(response_data, file)
-            print(f"Response saved for {api_name}")
+                json.dump(response_data, file, indent=4)
+            self.logger.log_info(f"Response saved for {api_name}")
         except Exception as e:
-            print(f"Error saving response for {api_name}: {e}")
+            self.logger.log_error(f"Error saving response for {api_name}: {e}")
 
     def load_response(self, api_name: str) -> dict:
         try:
@@ -21,8 +24,8 @@ class DataManager:
             with open(file_path, "r") as file:
                 return json.load(file)
         except FileNotFoundError:
-            print(f"No data found for {api_name}")
+            self.logger.log_warning(f"No data found for {api_name}")
             return {}
         except Exception as e:
-            print(f"Error loading response for {api_name}: {e}")
+            self.logger.log_error(f"Error loading response for {api_name}: {e}")
             return {}
